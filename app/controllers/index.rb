f6452b3dcaa5ce7e5d7f2376c7b9b60a5df39362
arianakvergara <ariana.vergara@gmail.com>
2014-10-30 13:36:51 -0700

    added @decks instance variable and potential set up for users controllers

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,21 @@
 get '/' do
-
+  @decks=Deck.all
   erb :home
+end
+
+#----------- USERS -----------
+
+get '/users/new' do
+  @user = User.new
+  erb :sign_up
+end
+
+post '/users' do
+  @user=User.new(params[:user])
+  if @user.save
+    redirect  '/'
+  else
+    @errors = @user.errors
+    erb :sign_up
+  end
 end
