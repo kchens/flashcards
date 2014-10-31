7815012547dcf6505c9964886c833606c043f863
chrisvall <christinevallaure@gmail.com>
2014-10-30 17:42:49 -0700

    finished sign_in and sign_up, changed controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 get '/' do
   @decks=Deck.all
   erb :home
@@ -11,11 +13,25 @@ get '/users/new' do
 end
 
 post '/users' do
-  @user=User.new(params[:user])
-  if @user.save
+  username = params["username"]
+  password = params["password"]
+  @user = User.new
+  # if @user.register(username, password)
+  if username #comment this out later
+    session[:username] = @user.username
     redirect  '/'
   else
     @errors = @user.errors
     erb :sign_up
   end
+end
+
+#----------- Login -----------
+
+get '/login' do
+  erb :sign_in
+end
+
+post '/login' do
+  #ariana has good stuff
 end
