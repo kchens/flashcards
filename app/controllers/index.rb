a6378954bc14e53ed4e70a7675c66aacb893dd09
arianakvergara <ariana.vergara@gmail.com>
2014-10-30 18:39:25 -0700

    this is the new controllers_file_work_with_this_one

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,22 +3,22 @@ get '/' do
   erb :home
 end
 
-get'/game/:id' do
-  if session[:value]
-    @id=params[:id]
-    erb :game
-  else
-    redirect to ('/login')
+get'/question/:id' do
+  # @question=Card.find(params[:id]).questions
+  @deck = Deck.find(params[:deck].to_i).cards.to_a.shuffle
+  #4 answers passed to the erb
+  @wrong_answers = []
+  3.times do
+    @wrong_answers << Card.find(rand(10)).answer
   end
+  @right_answer=
+  erb :qu_home
 end
 
-post '/questions' do
-  if session[:value]
-    puts params[:deck].to_i
-    Deck.find(params[:deck].to_i).cards.to_a.shuffle
-  else
-    redirect to('/login')
-  end
+post '/question/:id' do
+ @question = Card.find(params[:id]).questions
+ # @answer =
+ erb :qu_answer
 end
 
 #-------SESSIONS--------------------
@@ -27,9 +27,9 @@ get '/login' do
   erb :sign_in
 end
 
-post '/register' do
+post '/login' do
   # sign-in
-  @user=User.find(params[:username])
+  @user=User.find(username: params[:username])
   if @user.password == params[:password]
     #set the session
     session[:user_id] = @user.id
