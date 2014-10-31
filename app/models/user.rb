568317f2ff70ade24edb872e9570750b9d966f72
Murat Gocmen <murat.dbc@gmail.com>
2014-10-30 18:27:06 -0700

    added bcrypt and auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   has_many :scores
+
+  #
+  def register(username,password)
+    password_salt=BCrypt::Engine.generate_salt
+    p password_salt
+    password_hash=BCrypt::Engine.hash_secret(password,password_salt)
+    User.create!(username: username,salt: password_salt, password_hash: password_hash)
+  end
+
+  def authenticate(username,password)
+  User.where(username: username).exists?
+   if User.where(username: username).exists?
+    @user = User.find_by(username: username)
+       @user.password_hash == BCrypt::Engine.hash_secret(password, @user[:salt])
+      if @user.password_hash == BCrypt::Engine.hash_secret(password, @user[:salt])
+        return @user
+      end
+    end
+  end
 end
